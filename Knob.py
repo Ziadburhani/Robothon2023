e2c81eb3a51eb19c488f07bc0016ad4aad1e6f0e
Sameer Kishore <sameerkishore@gmail.com>
2023-04-28 22:38:01 +0400

    Modifies Knob.py to detect knobs using the new camera.

--- Knob.py
+++ Knob.py
@@ -1,35 +1,36 @@
 import cv2
 import numpy as np
 
-images_folder = 'images/'
-images = ('BOARD_20230425_182836.png'
-,'BOARD_20230425_182837.png'
-,'BOARD_20230425_182902.png'
-,'BOARD_20230425_182906.png'
-,'BOARD_20230425_182912.png'
-,'BOARD_20230425_182917.png'
-,'BOARD_20230425_182923.png'
-,'BOARD_20230425_182929.png'
-,'BOARD_20230425_182939.png'
-,'BOARD_20230425_182943.png'
-,'BOARD_20230425_182951.png'
-,'BOARD_20230425_182956.png'
-,'BOARD_20230425_183011.png'
-,'BOARD_20230425_183017.png'
-,'BOARD_20230425_183024.png'
-,'BOARD_20230425_183028.png'
-,'BOARD_20230425_183036.png'
-,'BOARD_20230425_183045.png'
-,'BOARD_20230425_183056.png'
-,'BOARD_20230425_183105.png'
-,'BOARD_20230425_183113.png'
-,'BOARD_20230425_183118.png'
-,'BOARD_20230425_183123.png'
-,'BOARD_20230425_183132.png'
-,'BOARD_20230425_183139.png'
-,'BOARD_20230425_183145.png'
-,'BOARD_20230425_183151.png'
-,'BOARD_20230425_183200.png')
+images_folder = 'Friday_images/'
+images = (
+    'BOARD_20230428_205304.png',
+    'BOARD_20230428_205307.png',
+    'BOARD_20230428_205312.png',
+    'BOARD_20230428_205314.png',
+    'BOARD_20230428_205315.png',
+    'BOARD_20230428_205319.png',
+    'BOARD_20230428_205324.png',
+    'BOARD_20230428_205329.png',
+    'BOARD_20230428_205333.png',
+    'BOARD_20230428_205338.png',
+    'BOARD_20230428_205343.png',
+    'BOARD_20230428_205348.png',
+    'BOARD_20230428_205358.png',
+    'BOARD_20230428_205404.png',
+    'BOARD_20230428_205413.png',
+    'BOARD_20230428_205419.png',
+    'BOARD_20230428_205427.png',
+    'BOARD_20230428_205434.png',
+    'BOARD_20230428_205439.png',
+    'BOARD_20230428_205443.png',
+    'BOARD_20230428_205450.png',
+    'BOARD_20230428_205455.png',
+    'BOARD_20230428_205459.png',
+    'BOARD_20230428_205507.png',
+    'BOARD_20230428_205511.png',
+    'BOARD_20230428_205516.png',
+    'BOARD_20230428_205524.png'
+)
 
 def show_image(title,file):
     cv2.imshow(title,file)
@@ -61,10 +62,10 @@ def detect_door(image):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # Apply Gaussian blur
-    blurred = cv2.GaussianBlur(gray, (11, 11), 0)
+    blurred = cv2.GaussianBlur(gray, (15, 15), 0)
 
     # Threshold with an optimal value
-    t,thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)
+    t,thresh = cv2.threshold(blurred, 128, 255, cv2.THRESH_BINARY)
 
     # Step 2: Morphological operations
     kernel = np.ones((5, 5), np.uint8)
@@ -79,13 +80,14 @@ def detect_door(image):
     for contour in contours:
         area = cv2.contourArea(contour)
         t_x, t_y, t_w, t_h = cv2.boundingRect(contour)
-        aspect_ratio = float(t_w)/t_h    
-        if 90000 < area < 180000 and 0.75 < aspect_ratio < 1.25:
+        aspect_ratio = float(t_w)/t_h  
+        if 140000 < area < 240000 and 0.75 < aspect_ratio < 1.5:
+            cv2.drawContours(image, [contour], -1, (0, 255, 0), 3)
             x = t_x
             y = t_y
             w = t_w
             h = t_h
-            break
+    #show_image('i', image)
     return x, y, w, h
 
 def detect_knob(image, x, y, w, h):
@@ -98,8 +100,8 @@ def detect_knob(image, x, y, w, h):
     # This is done so that the coordinates match the original image
     size = image.shape[0], image.shape[1], 3
     final = np.zeros(size, np.uint8)
-    final[y:y+w, x:x+h] = contr[y:y+w, x:x+h]
-    final = increase_contrast(final, 1.5)
+    final[y:y+h, x:x+w] = contr[y:y+h, x:x+w]
+    #final = increase_contrast(final, 1.5)
     
     # Convert to grayscale, apply a bilateral filter
     f_gray = cv2.cvtColor(final, cv2.COLOR_BGR2GRAY)
@@ -108,7 +110,7 @@ def detect_knob(image, x, y, w, h):
     # Find circles. Parameters here ensure that the circles are far apart
     # Of a certain size, and use the correct Canny thresholds
     rows = f_blur.shape[0]
-    circles = cv2.HoughCircles(f_blur, cv2.HOUGH_GRADIENT, 1, rows, param1=100, param2=10,minRadius=20, maxRadius=30)
+    circles = cv2.HoughCircles(f_blur, cv2.HOUGH_GRADIENT, 1, rows, param1=100, param2=10,minRadius=20, maxRadius=60)
     
     # If we've found even one circle, that's our guy.
     # Just look at the first and return the center and radius.
@@ -122,34 +124,39 @@ def detect_knob(image, x, y, w, h):
 
 
 #define a video capture object
-print("Starting camera")
+#print("Starting camera")
 #cam_device = 3 # on Judhi's laptop
-cam_device = 0 # on lab's desktop
+#cam_device = 0 # on lab's desktop
 #vid = cv2.VideoCapture(cam_device,cv2.CAP_DSHOW) # activate Windows Direct Show for faster camera setup
-vid = cv2.VideoCapture(0) # for other systems
+#vid = cv2.VideoCapture(0) # for other systems
 
-print("Setting video resolution")
-vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # max 3840 for 4K, 1920 for FHD
-vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # max 2160 for 4K, 1080 for FHD
+#print("Setting video resolution")
+#vid.set(cv2.CAP_PROP_FRAME_WIDTH, 1920) # max 3840 for 4K, 1920 for FHD
+#vid.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080) # max 2160 for 4K, 1080 for FHD
 
 # Capture the video frame
-ret, img = vid.read()
+#ret, img = vid.read()
 
-# Increase contrast
-contr = increase_contrast(img)
-
-# Find the door first
-x, y, w, h = detect_door(contr)
-
-# If a door is found, detect the knob and draw it on the original
-if x != 0:
-    # knob_center and knob_readius are your pixel values for the door-knob
-    # Maybe the radius isn't as important, but the center should be useful
-    knob_center, knob_radius  = detect_knob(img, x, y, w, h)
+for i in range(0, len(images)):
+    print("Opening image: {} - {} of {}".format(images[i],i+1, len(images)))
+    img = cv2.imread(images_folder + images[i])
     
-    # Draw both and show the image, just for fun.
-    if knob_radius != 0:
-        cv2.circle(img, knob_center, 5, (255, 0, 0), -1)
-        cv2.circle(img, knob_center, knob_radius, (0, 255, 0), 3)
-        show_image("result",img)
+    # Increase contrast
+    #contr = increase_contrast(img)
+    contr = img
+    # Find the door first
+    x, y, w, h = detect_door(contr)
 
+    # If a door is found, detect the knob and draw it on the original
+    if x != 0:
+        # knob_center and knob_readius are your pixel values for the door-knob
+        # Maybe the radius isn't as important, but the center should be useful
+        knob_center, knob_radius  = detect_knob(img, x, y, w, h)
+        
+        # Draw both and show the image, just for fun.
+        if knob_radius != 0:
+            cv2.circle(img, knob_center, 5, (255, 0, 0), -1)
+            cv2.circle(img, knob_center, knob_radius, (0, 255, 0), 3)
+            show_image(str(images[i]),img)
+    else:
+        print("Door not found.")
