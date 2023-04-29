10bfca86f4929e3d5156e99939d47dc3ceb70345
judhimdx <j.prasetyo@mdx.ac.ae>
2023-04-29 19:24:34 +0400

    translation calculation

--- translate.py
+++ translate.py
@@ -7,43 +7,37 @@ import numpy as np
 # gradX = 2.84 # take from cal_image_corrected
 # gradY = 2.44 # take from cal_image_corrected
 angle_deg = 0 # take from cal_image_corrected, add minus sign
-tl_X, tl_Y = 284, 318 # take from cal_image_corrected
-tr_X, tr_Y = 1268, 318
-bl_X, bl_Y = 284, 807
-br_X, br_Y = 1268, 807
+tl_x, tl_y = 284, 318 # take from cal_image_corrected
+tr_x, tr_y = 1268, 317
+bl_x, bl_y = 284, 807
+br_x, br_y = 1270, 808
 
 Y_len = 200
 X_len = 100
 
-gradX = X_len / (tr_X - tl_X)
-gradY = Y_len / (bl_Y - tl_Y)
+gradX = (tr_x - tl_x) / Y_len
+gradY = (bl_y - tl_y) / X_len
 
 print("gradX", gradX)
 print("gradY", gradX)
 
 def calculateXY(xc, yc):
-    xc = xc - tl_X # top left X pixel
-    yc = yc - tl_Y # top left Y pixel
-    calc_wx = -550 + round( yc * gradY, 2) # Y robot is x pixel
-    calc_wy = 50 + round( xc * gradX, 2) # X robot is y pixel 
+    xc = xc - tl_x # top left X pixel
+    yc = yc - tl_y # top left Y pixel
+    calc_wx = -550 + round( yc / gradY, 2) # Y robot is x pixel
+    calc_wy = 50 + round( xc / gradX, 2) # X robot is y pixel 
     return calc_wx, calc_wy
 
 px1, py1 = (1465, 302) # blue button pixel
-px2, py2 = (74, 271)
-px3, py3 = (1268,319)
+px2, py2 = (741, 271) # blue button pixel
 
-wx1, wy1 = calculateXY(px1,py1)
-wx2, wy2 = calculateXY(px2,py2)
-wx3, wy3 = calculateXY(px3,py3)
+wX1, wY1 = calculateXY(px1,py1)
+wX2, wY2 = calculateXY(px2,py2)
 
-print(wx1, wy1)
-print(wx2, wy2)
-print(wx3, wy3)
+print(wX1, wY1)
 
-actual_x1, actual_y1 = (-528.546, 276.747)
-actual_x2, actual_y2 = (-559.394, 144.089)
-actual_x3, actual_y3 = (-550, 250)
+actual_X1, actual_Y1 = (-554.315, 291.638)
+actual_X2, actual_Y2 = (-559.394, 44.089)
 
-print("Gap blue button", (actual_x1 - wx1, actual_y1 - wy1) )
-print("Gap knob ", (actual_x2 - wx2, actual_y2 - wy2) )
-print("Gap 3 ", (actual_x3 - wx3, actual_y3 - wy3) )
+print("Gap blue button", (actual_X1 - wX1, actual_Y1 - wY1) )
+
