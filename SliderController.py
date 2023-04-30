e6ec841012f4c35e2a3e6c87baec71792f5f4922
Sameer Kishore <sameerkishore@gmail.com>
2023-04-30 12:06:19 +0400

    Script works with new images. Adds an option for live_mode in SliderController.

--- SliderController.py
+++ SliderController.py
@@ -2,25 +2,30 @@ import cv2
 from slider_task import *
 
 DEBUG_MODE = True
+LIVE_MODE = False
 
-images_folder = 'Saturday_images/'
-#first_image = 'REDBOX_20230429_162715.png'
-vid = cv2.VideoCapture(0)
-while(vid.isOpened()):
+if LIVE_MODE is False:
+    images_folder = 'Saturday_images/'
+    first_image = 'WIN_20230430_00_39_36_Pro.jpg'
+    second_image = 'WIN_20230430_00_39_52_Pro.jpg'
+    first_image = cv2.imread(images_folder + first_image)
+else:
+    vid = cv2.VideoCapture(0)
+    while(vid.isOpened()):
         ret, img = vid.read()
         cv2.imshow('i', img)
         k = cv2.waitKey(20)
         if k == ord('q'):
             first_image = img
             break
-#first_image = cv2.imread()
-#second_image = 'REDBOX_20230429_162813.png'
 
-#first_arrow = cv2.imread(images_folder + first_image)
 first_arrow_distance = get_target(first_image, None, DEBUG_MODE)
 print("First arrow distance = {:.2f} mm".format(first_arrow_distance))
 
-while(vid.isOpened()):
+if LIVE_MODE is False:
+    second_image = cv2.imread(images_folder + second_image)   
+else:
+    while(vid.isOpened()):
         ret, img = vid.read()
         cv2.imshow('i', img)
         k = cv2.waitKey(20)
@@ -28,6 +33,5 @@ while(vid.isOpened()):
             second_image = img
             break
 
-#second_arrow = cv2.imread(images_folder + second_image)
 second_arrow_distance = get_target(second_image, first_image, DEBUG_MODE)
 print("Second arrow distance = {:.2f} mm".format(second_arrow_distance))
